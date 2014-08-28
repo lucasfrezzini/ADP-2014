4b664ecd994132288bbe9d25991753c2691aa1a3
lucaspowa <luquiitas.f@gmail.com>
2014-08-28 13:24:15 -0300

    Revert "correcciones"
    This reverts commit f88cb79d6c027d4a0fb3b033b0d65612d910ace3.

--- Práctica 2/p2ej3.pas
+++ Práctica 2/p2ej3.pas
@@ -4,8 +4,9 @@ mayor cantidad de ventas y la cantidad mínima vendida junto al día en que se p
 
 program eje3p2;
 	var
-		num, max, max2, min, dia, i: Integer;
+		total, num, max, max2, min, dia, i: Integer;
 	begin
+		total := 0;
 		max := -1;
 		min := 99999;
 		for i := 1 to 31 do begin
@@ -13,19 +14,10 @@ program eje3p2;
 			if (num > max) then begin
 				max2 := max;
 				max := num;
-				{ Máximo mayor }
-			end
-			else
-				if (num > max) then
-					max2 := num;
-					{ Máximo menor }
+			end;
 			if (num < min) then begin
 				min = num;
 				dia = i;
-				{ Mínimo y día de ese mínimo }
 			end;
 		end;
-		write('El Primer día de mayor venta, se obtuvo: ', max, );
-		write('El Segundo día de mayor venta, se obtuvo: ', max);
-		write('El día de peor venta fue: ', dia, 'con: ', max);
 	end.
