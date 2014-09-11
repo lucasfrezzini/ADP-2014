601b335ba98ab5dec8548fda29534c7ded958808
lucaspowa <luquiitas.f@gmail.com>
2014-09-11 15:13:47 -0300

    Comenzando Práctica 3

---
+++ Práctica 10/p10ej3.pas
@@ -0,0 +1,7 @@
+{
+a) Escriba un módulo que reciba una lista de strings L y un string S, y retorne verdadero si S está en L (falso,
+en caso contrario). La lista no se encuentra ordenada.
+
+b) Idem a), pero suponiendo que la lista está ordenada en forma ascendente. 
+Nota: El módulo debe detener la búsqueda al encontrar la primera ocurrencia de la palabra. 
+}
