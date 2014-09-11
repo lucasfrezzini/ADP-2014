64419ab3c092b4669b60518eb23ebed3ec9f4194
lucaspowa <luquiitas.f@gmail.com>
2014-09-11 16:05:36 -0300

    Ejercicio 3 Práctica 10
    Terminado

--- Práctica 10/p10ej3.pas
+++ Práctica 10/p10ej3.pas
@@ -5,3 +5,50 @@ en caso contrario). La lista no se encuentra ordenada.
 b) Idem a), pero suponiendo que la lista está ordenada en forma ascendente. 
 Nota: El módulo debe detener la búsqueda al encontrar la primera ocurrencia de la palabra. 
 }
+
+program ejercicio3;
+	type
+		lista = ^nodo;
+		nodo = record
+			dato : string;
+			sig: lista;
+		end;
+
+	function desordenada(l: lista; s: string): Boolean;
+		var
+			ok: Boolean;
+		begin
+			ok:= false;
+			while (l <> nil) and (not ok) do begin
+				if (l^.dato = s) then
+					ok:= true;
+				l:= l^.sig;
+			end;
+			if (ok) then
+				desordenada:= ok
+			else
+				desordenada:= ok;
+		end;
+
+	function ordenada(l: lista; s: string): Boolean;
+		begin
+			while (l <> nil) and (l^.dato < s) do
+				l:= l^.sig;
+			if (l^.dato = s) then
+				ordenada:= true
+			else
+				ordenada:= false;
+		end;
+
+	var
+		l, l2: lista;
+		s: string;
+	begin
+		l:= nil;
+		l2:= nil;
+		read(s);
+		crearListaDeStringOrdenada(l); {se dispone}
+		crearListaDeStringDesordenada(l2); {se dispone}
+		write('El valor está? ', desordenada(l2, s));
+		write('El valor está? ', ordenada(l, s));
+	end.
