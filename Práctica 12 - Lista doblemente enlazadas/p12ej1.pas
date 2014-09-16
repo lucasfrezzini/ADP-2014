feb387561475abef375d9a699e71716404bdf7c3
lucaspowa <luquiitas.f@gmail.com>
2014-09-16 12:07:00 -0300

    Ejercicio 1 Práctica 12
    Completo, modularizado.
    *NOTA*: Los numeros no vienen en orden fue una precondicion modificada
    para hacerlo más entretenido.

--- Práctica 12 - Lista doblemente enlazadas/p12ej1.pas
+++ Práctica 12 - Lista doblemente enlazadas/p12ej1.pas
@@ -5,6 +5,10 @@ una lista doblemente enlazada. Los números se leen en orden, por ejemplo: 1, 2,
 
 Finalizada la lectura, imprimir en pantalla los números en forma ascendente (de menor a mayor) 
 y descendente (de mayor a menor).
+
+############################################################################################
+### NOTA: Para complicar más el algoritmo, tome como que no vienen en orden los numeros. ###
+############################################################################################
 }
 
 program ejercicio1;
@@ -17,11 +21,81 @@ program ejercicio1;
 			ant: lista;
 		end;
 
-		punteros = record
-			pri_sig: lista;
-			pri_ant: lista;
+		listaDoble = record
+			pri: lista;
+			ult: lista;
 		end;
 
+	procedure crearListaOrdenada(var ld: listaDoble; num: Integer);
+		var
+			nuevo, act, ant: lista;
+		begin
+			new(nuevo);
+			nuevo^.num:= num;
+			nuevo^.sig:= nil;
+			nuevo^.ant:= nil;
+			if (ld.pri = nil) then begin
+				ld.pri:= nuevo;
+				ld.ult:= nuevo;
+			end
+			else begin
+				act:= ld.pri;
+				ant:= ld.pri;
+				while (act <> nil) and (act^.num < num) do begin
+					ant:= act^.num;
+					act:= act^.sig;
+				end;
+				if (act <> nil) then begin
+					nuevo^.sig:= act;
+					act^.ant:= nuevo;
+				end
+				else
+					ld.ult:= nuevo;
+				ant^.sig:= nuevo;
+				nuevo^.ant:= ant;				
+			end;
+				
+		end;
+
+	procedure leerNumeros(var ld: listaDoble);
+		var
+			num: Integer;
+		begin
+			read(num);
+			while (num <> 0) do begin
+				crearListaOrdenada(ld, num);
+				read(num);
+			end;
+		end;
+
+	procedure imprimirAsc(pri: lista);
+		begin
+			while (pri <> nil) do begin
+				write(pri^.num);
+				pri:= pri^.sig;
+			end;
+		end;
+
+	procedure imprimirDesc(ult: lista);
+		begin
+			while (ult <> nil) do begin
+				write(ult^.num);
+				ult:= ult^.ant;
+			end;
+		end;
+
+	procedure inicializarListaDoble(var ld: listaDoble);
+		begin
+			ld.pri:= nil;
+			ld.ult:= nil;
+		end;
+
+	var
+		ld: listaDoble;
+
 	begin
-		Main();
+		inicializarListaDoble(ld);
+		leerNumeros(ld);
+		imprimirAsc(ld.pri);
+		imprimirDesc(ld.ult);
 	end.
