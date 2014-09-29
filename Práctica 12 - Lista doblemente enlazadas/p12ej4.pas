c945c6eef23c07525b81a9d246445f5407cec3fc
lucaspowa <luquiitas.f@gmail.com>
2014-09-29 04:10:22 -0300

    Commit

--- Práctica 12 - Lista doblemente enlazadas/p12ej4.pas
+++ Práctica 12 - Lista doblemente enlazadas/p12ej4.pas
@@ -43,11 +43,108 @@ program ejercicio4;
 			pri_cod: lista;
 		end;
 
-	procedure insertar(var ld:listaDoble; e: empleado);
+	procedure leerEmpleado(var e: empleado);
 		begin
-			porNumEmp(ld.pri_numEmp, e);
-			porCod(ld.pri_cod, e)
+			read(e.numEmp);
+			read(e.nombre);
+			read(e.ape);
+			read(e.prof);
+			read(e.cod);
+			read(e.sueldo);
+			read(e.anti);
 		end;
+
+	procedure porNumEmp(var ld: listaDoble; nuevo: lista);
+		var
+			ant, act: lista;
+		begin
+			act:= ld.pri_numEmp;
+			while (act <> nil) and (act^.dato.numEmp < nuevo^.dato.numEmp) do begin
+				ant:= act;
+				act:= act^.sig_numEmp;
+			end;
+			if (act = ld.pri_numEmp;) then begin
+				nuevo^.sig_numEmp := ld.pri_numEmp;
+				ld.pri_numEmp := nuevo;
+			end
+			else begin
+				ant^.sig_numEmp:= nuevo;
+				nuevo^.sig_numEmp:= act;
+			end;
+		end;
+
+	procedure porCod(var ld: listaDoble; nuevo: lista);
+		var
+			ant, act: lista;
+		begin
+			act:= ld.pri_cod;
+			while (act <> nil) and (act^.dato.cod < nuevo^.dato.cod) do begin
+				ant:= act;
+				act:= act^.sig_cod;
+			end;
+			if (act = ld.pri_cod;) then begin
+				nuevo^.sig_cod := ld.pri_cod;
+				ld.pri_cod := nuevo;
+			end
+			else begin
+				ant^.sig_cod:= nuevo;
+				nuevo^.sig_cod:= act;
+			end;
+		end;
+
+	procedure insertarEmpleado(var ld:listaDoble; e: empleado);
+		var
+			nuevo: lista;
+		begin
+			new(nuevo);
+			nuevo^.dato:= e;
+			nuevo^.sig_cod:= nil;
+			nuevo^.sig_numEmp:= nil;
+			if (ld.pri_numEmp = nil) then begin
+				ld.pri_numEmp:= nuevo;
+				ld.pri_cod:= nuevo;
+			end
+			else begin
+				porNumEmp(ld, nuevo);
+				porCod(ld, nuevo;
+			end;
+		end;
+
+	procedure eliminarDepto(var ld: listaDoble;);
+		var
+			act: Integer;
+			aux: lista;
+		begin
+			aux:= ld^.pri_cod;
+			act:= aux.dato.cod;
+			while (aux <> nil) and (act <> 4) do begin
+				act:= aux^.dato.cod;
+				aux:= aux^.sig_cod;
+			end;
+			if (aux <> nil) then begin
+				while (act = 4) do begin
+					eliminarCod();
+					eliminarNro();
+					aux:= aux^.sig_cod;
+					act:= aux^.dato.cod;
+				end;
+			end;
+			aux:= ld^.pri_cod;
+			while (aux <> nil) and (act <> 10) do begin
+				act:= aux^.dato.cod;
+				aux:= aux^.sig_cod;
+			end;
+			if (aux <> nil) then begin
+				while (act = 10) do begin
+					eliminarCod();
+					eliminarNro();
+					aux:= aux^.sig_cod;
+					act:= aux^.dato.cod;
+				end;
+			end;
+		end;
+
+		{Mucho código para hacer algo tan sencillo, me aburrí. Despues lo veo, trankilo.}
 
 	begin
 		
---
+++ Práctica 13 - Arboles binarios/p13ej2.pas
@@ -0,0 +1,7 @@
+{
+Generar un árbol binario de búsqueda con nombres de personas que se leen desde teclado. La 
+lectura finaliza con el nombre ‘ZZZ’.  Una vez generado el árbol, realice un módulo que reciba el 
+árbol  y  un  nombre,  y  retorne  verdadero  si  existe  dicho  nombre  en  el  árbol  o  falso  en  caso 
+contrario
+}
+
